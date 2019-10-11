d25ab832bb6e556081ef7f8d51210d58afc14eed
StefanTreep <stefantreep@hotmail.com>
2019-10-11 13:58:18 +0200

    Create lecture11.py

---
+++ src/lecture11.py
@@ -0,0 +1,14 @@
+
+print(7 / 4) # / is division
+
+print(7 % 4) # % is modulus
+
+print(50 % 5) # 50 is divisible by 5
+
+print(20 % 2) # n mod 2 is a good way of checking whether a number is even
+
+print(2 ** 3) # ** is power
+
+print((2 + 10) * 3) # brackets work
+
+
