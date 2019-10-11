eb3a4b72ceff8b12493a88ed75f8cca8816c054b
StefanTreep <stefantreep@hotmail.com>
2019-10-11 13:58:14 +0200

    Create lecture10.py

---
+++ src/lecture10.py
@@ -0,0 +1,13 @@
+
+foo : int = 5
+print(foo)
+
+bar : dict = { 'one' : 1.0, 'two' : 2.0 }
+bar['one'] = 1.1
+print(bar['one'])
+
+baz : tuple = (10, "hello", 200.3)
+# baz[1] = "bye" # tuples are immutable
+print(baz)
+
+
