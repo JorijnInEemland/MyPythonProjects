a3732a4b6f219bcefa8783ade398007e8b6979fb
StefanTreep <stefantreep@hotmail.com>
2019-10-11 13:58:23 +0200

    Create lecture14.py

---
+++ src/lecture14.py
@@ -0,0 +1,23 @@
+
+# you can get chars from a string, starts at 0
+hello = "hello"
+print(hello[0])
+
+# Python strings support reverse indexing
+print(hello[-4])
+
+# slicing is cool, it works like this: your_string[start : stop : step]
+longer_string = "loNgeR sTriNg"
+print(longer_string[2:-1:3])
+
+# Python strings can use " and '
+jorijn = "I'm Jorijn"
+print(jorijn)
+
+# you can escape characters
+new_line = "\"new \\\nli\tne"
+print(new_line)
+
+# use len(your_string) to get its length
+print(len(hello))
+print(len(new_line))
