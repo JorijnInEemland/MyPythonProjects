46686b0ad9cbf851e72ebfca339905d5e4ff6232
StefanTreep <stefantreep@hotmail.com>
2019-10-11 14:41:36 +0200

    Create lecture17.py

---
+++ src/lecture17.py
@@ -0,0 +1,3 @@
+# another short faq
+# 1) strings are immutable (you can't use indexing to change individual characters)
+# 2) you use the # symbol to create comments in python (can't do multiline comments oof)
