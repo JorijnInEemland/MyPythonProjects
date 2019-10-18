098546bf5293b7b5ae23c950275821d9848934b5
StefanTreep <stefantreep@hotmail.com>
2019-10-18 12:30:52 +0200

    Create lecture023.py

---
+++ src/lecture023.py
@@ -0,0 +1,4 @@
+# another short faq
+# it says that dictionaries are mappings and do not retain order
+# if you want the capabilities of a dictionary but with ordering,
+# watch the ordereddict lecture later in the course
