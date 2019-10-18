e1c0ef55c51c270cd7eb2e43ac5e3a907d80f1fc
StefanTreep <stefantreep@hotmail.com>
2019-10-18 13:41:32 +0200

    Create lecture026.py

---
+++ src/lecture026.py
@@ -0,0 +1,11 @@
+# booleans are True or False
+
+# type and value
+print(type(True))
+print(False)
+print('')
+
+# example: equals, greater/smaller than, and an number
+print(2 == 5)
+print(3 > 1)
+print(bool('test'.count('t')))
