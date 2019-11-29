92200f8a1666f36c70a26c513316b04a259b7153
StefanTreep <stefantreep@hotmail.com>
2019-11-29 15:30:16 +0100

    Create lecture051.py

---
+++ section06/lecture051.py
@@ -0,0 +1,45 @@
+# Functions and Methods - Homework Assignment
+
+# Import the necessary stuff
+import math
+import string
+
+# Calculate the volume of a sphere given its radius
+def vol(rad):
+    return (4 / 3) * (math.pi) * (rad ** 3)
+
+# Check whether a number is in a given range
+def is_in_range(num, low, high):
+    return low <= num >= high
+
+# Count the amount of upper and lower case letters in a string
+# I tried to find how to do it with the collections module but couldn't
+def count_upper_lower(s):
+    u = len(list(filter(lambda c: c.isupper(), s)))
+    l = len(list(filter(lambda c: c.islower(), s)))
+    return u, l
+
+rogers = count_upper_lower('Hello Mr. Rogers, how are you this fine Tuesday?')
+print(f'Number of upper case characters : {rogers[0]}')
+print(f'Number of lower case characters : {rogers[1]}')
+
+# Check whether a string is a palindrome
+def palindrome(s):
+    s = list(filter(lambda c: c in string.ascii_letters, s.lower()))
+    l = len(s)
+    if l % 2 == 0:
+        return s[:l] == s[l::-1]
+    else:
+        return s[:l] == s[l+1::-1]
+
+print(f"'madam' {'is' if palindrome('madam') else 'is not'} a palindrome")
+print(f"'madamme' {'is' if palindrome('madamme') else 'is not'} a palindrome")
+print(f"'nurses run' {'is' if palindrome('nurses run') else 'is not'} a palindrome")
+print(f"'nurses walk' {'is' if palindrome('nurses walk') else 'is not'} a palindrome")
+
+# Check whether a string has every letter of the alphabet
+def pangram(s):
+    return not len(list(filter(lambda c: not c in s.lower(), string.ascii_lowercase)))
+
+print(f"'ewcpewtvnpuweipm' {'is' if pangram('ewcpewtvnpuweipm') else 'is not'} a pangram")
+print(f"'abcdefghijklmnopqrstuvwxyz' {'is' if pangram('abcdefghijklmnopqrstuvwxyz') else 'is not'} a pangram")
