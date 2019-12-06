6be89c0da4b81891ad7fc21c5f73e2c6a6fd4c54
StefanTreep <stefantreep@hotmail.com>
2019-12-06 12:20:08 +0100

    Improvements to the code

--- section06/lecture051.py
+++ section06/lecture051.py
@@ -10,7 +10,10 @@ def vol(rad):
 
 # Check whether a number is in a given range
 def is_in_range(num, low, high):
-    return low <= num >= high
+    return low <= num <= high
+
+print(f"10 {'is' if is_in_range(10, 5, 15) else 'is not'} in range 5 to 15")
+print(f"5 {'is' if is_in_range(5, 10, 15) else 'is not'} in range 10 to 15")
 
 # Count the amount of upper and lower case letters in a string
 # I tried to find how to do it with the collections module but couldn't
@@ -20,17 +23,13 @@ def count_upper_lower(s):
     return u, l
 
 rogers = count_upper_lower('Hello Mr. Rogers, how are you this fine Tuesday?')
-print(f'Number of upper case characters : {rogers[0]}')
-print(f'Number of lower case characters : {rogers[1]}')
+print(f"Number of upper case characters : {rogers[0]}")
+print(f"Number of lower case characters : {rogers[1]}")
 
 # Check whether a string is a palindrome
 def palindrome(s):
     s = list(filter(lambda c: c in string.ascii_letters, s.lower()))
-    l = len(s)
-    if l % 2 == 0:
-        return s[:l] == s[l::-1]
-    else:
-        return s[:l] == s[l+1::-1]
+    return s == s[::-1]
 
 print(f"'madam' {'is' if palindrome('madam') else 'is not'} a palindrome")
 print(f"'madamme' {'is' if palindrome('madamme') else 'is not'} a palindrome")
